1d42087a4ca86ae7fa47336f728a117e910f9af6
Michael <wil13040@byui.edu>
2019-05-01 16:15:59 -0600

    Team Activity 2

---
+++ web/ta2js.js
@@ -0,0 +1,11 @@
+function clicked() {
+    alert("Clicked!");
+}
+function colors() {
+    var nColor = document.getElementById(newColor);
+
+    var div = document.getElementById(div1);
+
+    var color = textbox.value;
+    div.style.backgroundColor = color;
+}
