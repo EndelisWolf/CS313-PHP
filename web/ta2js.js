cce95e611a4012cda012aa6dcc004c51fab73090
Michael <wil13040@byui.edu>
2019-05-01 18:08:19 -0600

    edited Team Activity 2: Edit 5

--- web/ta2js.js
+++ web/ta2js.js
@@ -1,17 +1,14 @@
-// ta02.js
-function clickMe() {
-	alert("Clicked!");
+function clicked() {
+    alert("Clicked!");
 }
 
-function changeColor() {
-	var textbox_id = "txtColor";
-	var textbox = document.getElementById(textbox_id);
+function colors() {
+    var nColor = "newColor"; 
+    var colorNew = document.getElementById(nColor);
 
-	var div_id = "div1";
-	var div = document.getElementById(div_id);
+    var newDiv = "div1";
+    var div = document.getElementById(newDiv);
 
-	// We should verify values here before we use them...
-	var color = textbox.value;
-	div.style.backgroundColor = color;
-
+    var color = colorNew.value;
+    div.style.backgroundColor = color;
 }
