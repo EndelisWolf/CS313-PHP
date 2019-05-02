7181312fdc6a7ccffe4940b5306ee8f14b866306
Michael <wil13040@byui.edu>
2019-05-01 18:27:32 -0600

    edited Team Activity 2

--- web/ta2js.js
+++ web/ta2js.js
@@ -15,6 +15,10 @@ function colors() {
 
 $(document).ready(function(){
     $("button").click(function(){
-      $("#div3").fadeToggle(5000);
+      $("#div1").fadeToggle();
+      $("#div2").fadeToggle("slow");
+      $("#div3").fadeToggle(3000);
     });
   });
+
+  
