08099de551f4ecee04ed37c8291e6806d884ce46
Michael <wil13040@byui.edu>
2019-05-01 18:29:05 -0600

    edited Team Activity 2

--- web/ta2js.js
+++ web/ta2js.js
@@ -12,13 +12,3 @@ function colors() {
     var color = colorNew.value;
     div.style.backgroundColor = color;
 }
-
-$(document).ready(function(){
-    $("button").click(function(){
-      $("#div1").fadeToggle();
-      $("#div2").fadeToggle("slow");
-      $("#div3").fadeToggle(3000);
-    });
-  });
-
-  
