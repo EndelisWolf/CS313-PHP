0ccf25257dd09dfa7ab0ffc6616f94e9cb71e7ac
Michael <wil13040@byui.edu>
2019-05-03 15:43:47 -0600

    Large Changes made to homepage

--- web/Homepage.php
+++ web/Homepage.php
@@ -33,7 +33,7 @@
             <li onclick="clicked2()">Gaming</li>
             <div><img id="game" src="./game.JPG"></div>
             <li onclick="clicked3()">Interacting with friends</li>
-            <div><img id="friends" src="./friends.JPG"></div>
+            <div><img id="friend" src="./friends.JPG"></div>
         </ol>
         <br>
         To expalin each briefly:
