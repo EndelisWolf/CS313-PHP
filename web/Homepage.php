b2155d40495b019f83660a652d59a86bd590ac01
Michael <wil13040@byui.edu>
2019-05-03 15:38:29 -0600

    Large Changes made to homepage

--- web/Homepage.php
+++ web/Homepage.php
@@ -29,9 +29,9 @@
         <br>
         <ol>
             <li onclick="clicked()">Dogs & Wolfs</li>
-            <div><img id="doggo" src="./puppy3.JPG"></div>
+            <div><img id="doggo" src="./puppy3.jpg"></div>
             <li onclick="clicked2()">Gaming</li>
-            <div><img id="game" src="./game.jpg"></div>
+            <div><img id="game" src="./game.JPG"></div>
             <li onclick="clicked3()">Interacting with friends</li>
         </ol>
         <br>
