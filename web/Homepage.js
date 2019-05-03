4f66cdd57a23f873571dffb072c021ea56364b25
Michael <wil13040@byui.edu>
2019-05-03 08:11:07 -0600

    Large Changes made to homepage

--- web/Homepage.js
+++ web/Homepage.js
@@ -3,3 +3,11 @@
 function clicked() {
     document.getElementById("doggo").style = "display:block";
 }
+
+function clicked1() {
+    document.getElementById("game").style = "display:block";
+}
+
+function clicked2() {
+    document.getElementById("friend").style = "display:block";
+}
--- web/Homepage.php
+++ web/Homepage.php
@@ -13,7 +13,7 @@
 
     $welcome = date("m/d/yy h:m:s");
 
-    echo "<h1>Welcome to My Homepage! You arrived here at: $welcome <br>DISCLAIMER: It's this time somewhere I think.</h1>"
+    echo "<h1>Welcome to My Homepage! You arrived here at: $welcome <br>DISCLAIMER: It's this time un the near future.</h1>"
 
     ?>
     <h1>Who am I?</h1>
@@ -30,26 +30,28 @@
         <ol>
             <li onclick="clicked()">Dogs & Wolfs</li>
             <div><img id="doggo" src="./puppy3.jpg"></div>
-            <li>Gaming</li>
-            <li>Interacting with friends</li>
+            <li onclick="clicked2()">Gaming</li>
+            <div><img id="game" src="./game.jpg"></div>
+            <li onclick="clicked3()">Interacting with friends</li>
+            <div><img id="friend" src="./friend.jpg"></div>
         </ol>
         <br>
         To expalin each briefly:
         <br>
         <ol>
-            <li>
+            <li id="hov1">
                 I have aways found Dogs and & Wolfs absolutely amazing to watch. They act in manners that are to get
                 closer to their owners or to support one another, and tend to make sure that those that they care about
-                are taken care of. Another point is tha tDogs really are intelligent, and are able tell if you are sad,
+                are taken care of. Another point is that Dogs really are intelligent, and are able tell if you are sad,
                 happy, or need a way to get rid of excess energy and are willing to help you everyday no matter way.
             </li>
-            <li>
+            <li id="hov2">
                 I love playing games, they are able to put you in another world and interact with it, though its even
                 better with friends as it will allow you to play with friends that are gone over extremely long
                 distances if you play games online. It removes barriers that were once there for when an individual
                 would move.
             </li>
-            <li>
+            <li id="hov3">
                 People are social creatures, and even for someone as shy as me, we have to interact with others. The
                 significance for myself is tha tI am able to open up, and find new parts of my own life that I never
                 knew because my friends allow me to bring them out without worry in the end. It's a giant blessing when
