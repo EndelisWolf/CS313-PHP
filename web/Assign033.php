f32c1678249c237ddf4ea4bd9a14292d52b9f692
Michael <wil13040@byui.edu>
2019-05-09 16:26:14 -0600

    change of folders

--- web/Assign033.php
+++ web/Assign033.php
@@ -3,4 +3,9 @@
 
     session_start();
 
+    $_SESSION["Everday Bagel"] = $_POST["EB"];
+    $_SESSION["Plain Bagel"] = $_POST["PB"];
+    $_SESSION["Cinnamon Raisin Bagel"] = $_POST["CRB"];
+    $_SESSION["Asiago Bagel"] = $_POST["AB"];
+    $_SESSION["Blueberry Bagel"] = $_POST["BB"];
 ?>
