9f871155327cf4aae6a037985b05f5aa409c1911
Michael <wil13040@byui.edu>
2019-05-10 18:54:49 -0600

    Testing Text Boxes of 033

--- web/Assign032.php
+++ web/Assign032.php
@@ -3,11 +3,11 @@
 
     session_start();
 
-    $_SESSION["Everday Bagel"] = $_POST["EB"];
+    /*$_SESSION["Everday Bagel"] = $_POST["EB"];
     $_SESSION["Plain Bagel"] = $_POST["PB"];
     $_SESSION["Cinnamon Raisin Bagel"] = $_POST["CRB"];
     $_SESSION["Asiago Bagel"] = $_POST["AB"];
-    $_SESSION["Blueberry Bagel"] = $_POST["BB"];
+    $_SESSION["Blueberry Bagel"] = $_POST["BB"];*/
 
 ?>
 <!DOCTYPE html>
