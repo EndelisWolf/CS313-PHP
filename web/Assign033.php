23a77f4255f2c9b559e55464fee2eb1e8a8ff46d
Michael <wil13040@byui.edu>
2019-05-10 18:29:24 -0600

    Testing Text Boxes of 033

--- web/Assign033.php
+++ web/Assign033.php
@@ -23,8 +23,23 @@
 </head>
 
 <body>
-
+<h1>Where are we shipping?</h1>
 <form method="Form" action="Assign034.php">
+
+<label>First Name:</label>
+<input type="text" name="fName">
+<label>Last Name:</label>
+<input type="text" name="lName">
+<label>Stree Address:</label>
+<input type="text" name="sAddress">
+<label>Apartment Number(if Applicable)</label>
+<input type="number" name="aptNum">
+<label>City:</label>
+<input type="text" name="city">
+<label>State:</label>
+<input type="text" name="state">
+<label>Zipcode:</label>
+<input type="number" name="zipCode">
 </form>
 
 <button onclick="window.location.href = 'Assign032.php';">Back to 32</button>
