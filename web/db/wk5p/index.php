babbf4d6d1feafcd3973ddf9ebb887ef335d9110
Michael <wil13040@byui.edu>
2019-05-23 13:22:03 -0600

    Added the values

--- web/db/wk5p/index.php
+++ web/db/wk5p/index.php
@@ -15,10 +15,19 @@ session_start();
     </head>
  <body>
      <div>
-        <!-- <form action="" method="get">
+        <form action="display.php" method="get">
 
-         </form>-->
+         </form>
          <?php
+
+    echo "<select>";
+    foreach ($db->query('SELECT id FROM list') as $row)
+    {
+        echo "<option value='".$row['id']."'>".$row['id']."</option>";
+    }
+    echo "</seclect>";
+
+/* For reference
 
 foreach ($db->query('SELECT name, username, password FROM users') as $row)
 {
@@ -26,7 +35,7 @@ foreach ($db->query('SELECT name, username, password FROM users') as $row)
   echo 'user: ' . $row['username'];
   echo ' password: ' . $row['password'];
   echo '<br/>';
-}
+}*/
 
          ?>
      </div>
