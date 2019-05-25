2c7682729f0fd5d6a8ef68d2d79d05a872f68e2f
Michael <wil13040@byui.edu>
2019-05-25 09:21:56 -0600

    Testing output items

--- web/db/wk5p/display.php
+++ web/db/wk5p/display.php
@@ -7,18 +7,18 @@ include 'db.php';
 <html>
 
 <head>
-<title>To-Do To-Day Results</title>
+    <title>To-Do To-Day Results</title>
 </head>
 
 <body>
     <?php
     if (isset($_GET['listSelect']))
     {
-        $listItem = $_GET['listSelect'];
-        $result = 'SELECT dueDate, noteEntry FROM list WHERE id='.$listItem;
-        echo ($result);
-        $note = $db->query($result);
-        while ($row = $note->fetch(PDO::FETCH_ASSOC))
+        $note = $_GET['listSelect'];
+        $sql = 'SELECT dueDate, noteEntry FROM list WHERE id='.$note;
+        echo ($sql);
+        $statement = $db->query($sql);
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         {
             echo $row['dueDate'] . " " . $row['noteEntry'];
         }
