9fae2bc4dc4da4b987bbaf4033befda314b96d33
Michael <wil13040@byui.edu>
2019-05-25 09:56:14 -0600

    Attempting to show whats in the while

--- web/db/wk5p/display.php
+++ web/db/wk5p/display.php
@@ -1,6 +1,7 @@
 <?php 
 include 'db.php';
 
+session_start();
 ?>
 
 <!doctype html>
@@ -17,9 +18,11 @@ include 'db.php';
         $note = $_GET['listSelect'];
         /*$sql = 'SELECT dueDate, noteEntry FROM list WHERE id='.$note;
         echo ($sql);*/
+        //Due to using options to grab data, id=.note is for selecting the proper number id from the choices availible
         $statement = $db->query('SELECT dueDate, noteEntry FROM list WHERE id='.$note);
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         {
+            //everything that is not row is properly outputing.
             echo "Inside while<br />";
             echo $row['dueDate'] . " " . $row['noteEntry'] . "Rows should be here<br />";
             echo "Other side of rows!";
--- web/db/wk5p/index.php
+++ web/db/wk5p/index.php
@@ -15,7 +15,7 @@ session_start();
     </head>
  <body>
      <div>
-        <form action="display.php" method="get">
+        <form action="display.php" method="GET">
         Choose your note:<br>
          <?php
 
