0273633329265d1ec778170ff61567c720d9f192
Michael <wil13040@byui.edu>
2019-06-06 13:18:37 -0600

    Working on login

--- web/db/wk7p/login.php
+++ web/db/wk7p/login.php
@@ -6,6 +6,15 @@ session_start();
 
 <?php
     //$user = htmlspecialchars$_POST[];
+    if(isset($_POST))
+    {
+       $req = $db->query('SELECT id, name, username, password FROM users WHERE username = '.$_POST["uname"]);
+       $logn = $req->fetch(PDO::FETCH_ASSOC);
+       if (length($logn) < 1)
+       {
+           echo "Invalid entrie(s)!";
+       }
+    }
 
 
 ?>
@@ -25,15 +34,15 @@ session_start();
         <h3>Please enter your First Name, User Name, and Password</h3>
         <!-- a form should be here, but currently that won't be added due to additonal research you need before confirming that -->
         <!--<form action="index.php" method="POST"></form> Are two required for Creating an account and going to an account? Research that -->
-        <form action="index.php" method="POST">
-            <span>Name: </span><input type="text" size="10" maxlength="20" placeholder="First Name" required>
+        <form action="login.php" method="POST">
+            <span>Name: </span><input type="text" size="10" maxlength="20" placeholder="First Name" name="fName" required>
             <br />
             <span>Username: </span><input type="text" size="10" maxlength="16" minlength="3"
-                placeholder="3 - 16 Characters" required>
+                placeholder="3 - 16 Characters" name="uName" required>
             <!-- Java script function sohuld be included here to make sure no two usernames can be the same -->
             <br />
             <span>Password: </span><input type="text" size="10" maxlength="16" minlength="3"
-                placeholder="3 - 16 Characters" required>
+                placeholder="3 - 16 Characters" name="pwd" required>
             <br />
             <input type="submit" value="Login">
             
